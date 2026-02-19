0458975d2594e5ceef4c07ee39120891274b8c86
Doyoon Han <dyhan001@gmail.com>
2026-02-19 16:35:02 +1100

    fixed content&added Chapter3

---
+++ Chapter3/dynamic_memory_bakery/dynamic_memory_bakery.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int customer_num = 0;
+
+	cout << "Enter the number of customers: ";
+	cin >> customer_num;
+
+	string *bread = new string[customer_num];
+
+	cout << endl << "Bread made" << endl;
+
+	for (int i = 0; i < customer_num; i++) {
+		bread[i] = "bread_" + to_string(i + 1);
+		cout << *(bread + i) << endl;
+	}
+
+	delete[] bread;
+
+
+	return 0;
+}
---
+++ Chapter3/new_and_delete/new_and_delete.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// dynamic memory allocation
+	int *pt_int_value = new int;
+
+	*pt_int_value = 100;
+	cout << *pt_int_value << endl;
+
+	delete pt_int_value;
+	return 0;
+}
